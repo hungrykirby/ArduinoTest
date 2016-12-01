a7863718087d4aead105d9aa5188cb0b635ca5eb
Tutanrhamun <katsuki.hanawa.job@gmail.com>
2016-12-01 16:36:48 +0900

    for heroku

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var five = require('johnny-five');
 var board = new five.Board();
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(process.env.PORT || 8000);
+console.log('Server running');
 
 board.on('ready', function() {
   var led = new five.Led(13);
